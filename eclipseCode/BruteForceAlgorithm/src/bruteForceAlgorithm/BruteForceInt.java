70ed320bd196b2a39598fcdac23bd6ce3e89b7f8
chenfeng <chenfeng@iastate.edu>
2016-06-24 18:46:25 -0500

    add ACM_ExtraCredit problem & answer # H

---
+++ eclipseCode/BruteForceAlgorithm/src/bruteForceAlgorithm/BruteForceInt.java
@@ -0,0 +1,14 @@
+package bruteForceAlgorithm;
+
+public class BruteForceInt {
+
+	private int[] arr;
+
+	public BruteForceInt() {
+		arr = new int[10];
+	}
+	public void BFPrint(){
+		
+	}
+
+}
---
+++ eclipseCode/BruteForceAlgorithm/src/bruteForceAlgorithm/Test.java
@@ -0,0 +1,12 @@
+package bruteForceAlgorithm;
+
+import java.util.Arrays;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3};
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
