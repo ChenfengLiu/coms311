8507bb7f3aec10d7a08968c7d98ca6c06df87b61
chenfeng <chenfeng@iastate.edu>
2016-06-26 15:44:39 -0500

    Add permission() method in TimetoGraduate.java. Add combination(), combinationCal(), factorial() methods in Test.java, still testing combination() method.

--- eclipseCode/TimetoGraduate/src/timetoGraduate/Test.java
+++ eclipseCode/TimetoGraduate/src/timetoGraduate/Test.java
@@ -1,12 +1,97 @@
 package timetoGraduate;
 
+import java.util.Arrays;
+
 public class Test {
-	
+	private static int count = 0;
+
 	public static void main(String[] args) {
-		re(0);
+		// Course a = new Course(0, "F", 0, new int[0]);
+		//
+		// Course b = new Course(1, "S", 1, new int[] { 0 });
+		// Course c = new Course(2, "B", 2, new int[] { 0, 1 });
+		//
+		// int semester = 2;
+		// boolean[] finished = new boolean[3];
+		// finished[0] = true;
+		// finished[1] = true;
+		// finished[2] = false;
+		//
+		// System.out.println(permission(a,semester,finished));
+
+		int[] arr = { 0, 1, 2, 3, 4 };
+		int k = 3;
+		int numRows = combinationCal(arr.length, k);
+		System.out.println("numRows: " + numRows);
+
+		int[][] result = new int[numRows][k];
+		result = combination(arr, k, 0, result);
+		System.out.println(Arrays.deepToString(result));
 	}
 
-	public static void re(int semester) {
+	public static boolean permission(Course c, int semester, boolean[] finished) {
+		// check if already finished
+		boolean isFinished = finished[c.getName()];
+		// check semester
+		boolean isTime = (c.getSemester() == semester % 2) || (c.getSemester() == 2);
+		// check pre-request
+		boolean isQualified = true;
+		for (int i = 0; i < c.getNumPreRequest(); i++) {
+			if (!finished[c.getPreRequest(i)]) {
+				isQualified = false;
+				break;
+			}
+		}
+		System.out.println("isFinished: " + isFinished);
+		System.out.println("isTime: " + isTime);
+		System.out.println("isQualified: " + isQualified);
+		if (!isFinished && isTime && isQualified) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static int[][] combination(int[] arr, int k, int from, int[][] result) {
+		if (k == 0) {
+			count = count + 1;
+
+			return result;
+		}
+		for (int i = from; i <= arr.length - k; i++) {
+			result[count][result[count].length - k] = arr[i];
+			combination(arr, k - 1, i + 1, result);
+		}
+		return result;
+
+	}
+
+	/**
+	 * Calculate the integer value of the combination function nCk (choose k
+	 * from n)
+	 * 
+	 * @param n
+	 * @param k
+	 * @return
+	 */
+	private static int combinationCal(int n, int k) {
+		return factorial(n) / (factorial(k) * factorial(n - k));
+
+	}
+
+	/**
+	 * Calculate the factorial of an integer
+	 * 
+	 * @param i
+	 *            - a nonnegative integer
+	 * @return - the factorial of integer "i"
+	 */
+	private static int factorial(int i) {
+		if (i == 0) {
+			return 1;
+		} else {
+			return i * factorial(i - 1);
+		}
 
 	}
 
--- eclipseCode/TimetoGraduate/src/timetoGraduate/TimetoGraduate.java
+++ eclipseCode/TimetoGraduate/src/timetoGraduate/TimetoGraduate.java
@@ -11,19 +11,19 @@ public class TimetoGraduate {
 		Scanner scan = new Scanner(System.in);
 
 		while (true) {
-			
+
 			int numClass, maxClass;
 			System.out.println("Please enter you input below: ");
 			numClass = scan.nextInt();
 			maxClass = scan.nextInt();
 			System.out.println("numClass is: " + numClass);
 			System.out.println("maxClass is: " + maxClass);
-			
-			if(numClass == -1 && numClass == -1){
+
+			if (numClass == -1 && numClass == -1) {
 				scan.close();
 				break;
 			}
-			
+
 			ArrayList<Course> classList = new ArrayList<>();
 			String[] className = new String[numClass];
 
@@ -37,8 +37,7 @@ public class TimetoGraduate {
 			for (int i = 0; i < numClass; i++) {
 				String Cname = scan.next();
 				// find the course name in className, obtain index j, which is
-				// the
-				// alternative course name
+				// the alternative course name
 				for (int j = 0; j < numClass; j++) {
 					if (className[j].equals(Cname)) {
 						// set semester: F=0,S=1,B=2
@@ -53,8 +52,7 @@ public class TimetoGraduate {
 						for (int k = 0; k < numPrerequest; k++) {
 							String preName = scan.next();
 							// find the course name in className, obtain index
-							// p,
-							// which is the alternative course name
+							// p, which is the alternative course name
 							for (int p = 0; p < numClass; p++) {
 								if (className[p].equals(preName)) {
 									preRequest[k] = p;
@@ -76,17 +74,49 @@ public class TimetoGraduate {
 				System.out.print(classList.get(s).getName() + " " + classList.get(s).getSemester() + " "
 						+ classList.get(s).getNumPreRequest() + " ");
 				System.out.println(Arrays.toString(classList.get(s).getPreRequest()));
-			}//End for #5
-			
-			//TODO: given an arrayList of n course objects, find time to graduate
+			} // End for #5
+
+			// TODO: given an arrayList of n course objects, find time to
+			// graduate
 			System.out.println("Please wait till calculation is done.");
-			//TODO: create adjMatrix from given data (classList, numClass, maxClass), then brute force
+			boolean[] finished = new boolean[numClass];
+			// initialize boolean array: finished
+			for (int i = 0; i < numClass; i++) {
+				finished[i] = false;
+			}
+			// TODO: create adjMatrix from given data (classList, numClass,
+			// maxClass), then brute force
 			System.out.println("calculation done.");
-			
-		}//End while loop
-		
+
+		} // End while loop
+
 		scan.close();
-		
+
+	}
+
+	public static boolean permission(Course c, int semester, boolean[] finished) {
+		// check if already finished
+		boolean isFinished = finished[c.getName()];
+		// check semester
+		boolean isTime = (c.getSemester() == semester % 2) || (c.getSemester() == 2);
+		// check pre-request
+		boolean isQualified = true;
+		for (int i = 0; i < c.getNumPreRequest(); i++) {
+			if (!finished[c.getPreRequest(i)]) {
+				isQualified = false;
+				break;
+			}
+		}
+		if (!isFinished && isTime && isQualified) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static int[][] combination(int[] candidates, int maxClass) {
+		int[][] result = new int[1][maxClass];
+		return result;
 	}
 
 }
